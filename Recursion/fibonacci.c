98a63e173fb300e349a077161da98c49bfe429b0
Sawad <mdsawad19066@gmail.com>
2026-06-27 18:49:25 +0600

    Recursion Problems

---
+++ Recursion/fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int fibo(int n){
+    if(n==0)
+        return 0;
+    else if (n==1)
+        return 1;
+    else fibo(n-1) + fibo(n-2); 
+}
+
+int main(){
+    int n,i;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        printf("%d ", fibo(i));
+    }
+    return 0;
+}
