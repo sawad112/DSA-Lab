ae5d42dc1ae255c82a7d57e3a1797173539b99a4
Sawad <mdsawad19066@gmail.com>
2026-07-17 22:57:51 +0600

    Stack Implementation Using Linked List

--- Stack/stack_using_linkedlist.c
+++ Stack/stack_using_linkedlist.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node
-{
+typedef struct Node{
     int val;
     struct Node *next;
 } Node;
 
-typedef struct stack
-{
+typedef struct stack{
     Node *head;
     int cnt;
 } stack;
 
-stack *create()
-{
+stack *create(){
     stack *S = (stack *)malloc(sizeof(stack));
     S->head = NULL;
     S->cnt = 0;
     return S;
 }
 
-void push(stack *S, int v)
-{
+void push(stack *S,int v){
     Node *nNode = (Node *)malloc(sizeof(Node));
     nNode->val = v;
     nNode->next = S->head;
@@ -30,8 +26,7 @@ void push(stack *S, int v)
     S->cnt++;
 }
 
-int pop(stack *S)
-{
+int pop(stack *S){
     if (S->head == NULL)
     {
         printf("Empty\n");
@@ -45,8 +40,7 @@ int pop(stack *S)
     return val;
 }
 
-int top(stack *S)
-{
+int top(stack *S){
     if (S->head == NULL)
     {
         printf("Empty\n");
@@ -55,34 +49,27 @@ int top(stack *S)
     return S->head->val;
 }
 
-int size(stack *S)
-{
+int size(stack *S){
     return S->cnt;
 }
 
-int isEmpty(stack *S)
-{
+int isEmpty(stack *S){
     return S->head == NULL;
 }
 
-void free_s(stack *S)
-{
+void free_s(stack *S){
     Node *temp;
-
     while (S->head != NULL)
     {
         temp = S->head;
         S->head = S->head->next;
         free(temp);
     }
-
     free(S);
 }
 
-int main()
-{
+int main(){
     stack *S = create();
-
     push(S, 4);
     push(S, 5);
     push(S, 1);
@@ -97,6 +84,5 @@ int main()
     printf("Popped = %d\n", pop(S));
 
     free_s(S);
-
     return 0;
 }
